80d3f54e590bfe0b1829cad94537fa7a3f037154
diegofigs <12281609+diegofigs@users.noreply.github.com>
2022-12-03 14:54:34 -0400

    feat: add vercel deployment for client

--- client/src/server.js
+++ client/src/server.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const server = axios.create({
-  baseURL: "http://localhost:3042/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3042/api",
 });
 
 export default server;
